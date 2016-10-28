102da0b89e720a7d88c4eec1dc98df09a056aba0
Douglas Paul <douglas.w.paul@gmail.com>
2016-10-28 14:16:04 -0400

    Rename dist/ to public/

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -8,7 +8,7 @@ module.exports = new Config().merge({
     },
 
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'public'),
         publicPath: '/',
         filename: '[name].js'
     },
